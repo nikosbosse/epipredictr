8537356bfc1091a97bbab2477d7adb9cc1af2681
nikosbosse <nikosbosse@gmail.com>
2020-02-11 23:05:18 +0000

    initial commit

---
+++ R/epipredictr-package.R
@@ -0,0 +1,16 @@
+#' The 'epipredictr' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name epipredictr-package
+#' @aliases epipredictr
+#' @useDynLib epipredictr, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.19.3. https://mc-stan.org
+#'
+NULL
